f1241ef210bc4429eea81145759f6764191f9bc9
Aremu Smog <smog@web-boss.com.ng>
2019-06-28 10:17:03 +0100

    Add Seamless Computer Experience Section

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Computer Doctor</title>
+	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+	<div class="landing">
+		<div class="container">
+			<h1>We are fixing Africa, PC by PC</h1>
+		</div>
+	</div>
+</body>
+</html>
